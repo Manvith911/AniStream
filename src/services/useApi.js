d95a096a5dbe78af7a022e4793d9f711bbf0905c
Manvith <manvithudupa073@gmail.com>
2025-10-17 05:43:30 +0530

    Update useApi.js

--- src/services/useApi.js
+++ src/services/useApi.js
@@ -22,7 +22,7 @@ const fetchData = async (url) => {
 export const useApi = (endpoint, options = {}) => {
   return useQuery({
     queryKey: [endpoint],
-    queryFn: () => fetchData(`${endpoint}`), // prevent cache
+    queryFn: () => fetchData(`animes/${endpoint}`), // prevent cache
     retry: 2,
     enabled: !!endpoint,
     refetchOnWindowFocus: false,
