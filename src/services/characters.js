eefe8d726a8460e8af77a06e1770fa4a1df9a93a
Manvith <manvithudupa073@gmail.com>
2025-10-19 07:12:49 +0530

    Update characters.js

--- src/services/characters.js
+++ src/services/characters.js
@@ -1,22 +1,18 @@
-// src/services/characters.js
 export const fetchRandomCharacter = async () => {
   const query = `
     query ($page: Int, $perPage: Int) {
       Page(page: $page, perPage: $perPage) {
         characters {
           id
-          name {
-            full
-          }
-          image {
-            large
-          }
+          name { full }
+          image { large }
         }
       }
     }
   `;
   const randomPage = Math.floor(Math.random() * 50) + 1;
   const variables = { page: randomPage, perPage: 1 };
+
   try {
     const res = await fetch("https://graphql.anilist.co", {
       method: "POST",
