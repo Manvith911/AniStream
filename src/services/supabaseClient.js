ef62533fe90d3471bb4a796adacce2b1521e5845
Manvith <manvithudupa073@gmail.com>
2025-10-19 07:18:16 +0530

    Delete src/services/supabaseClient.js

--- src/services/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const SUPABASE_URL = "https://kgwtwcchttcamekzqqjx.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_3t9UEKWzWSPxcLKX8Xwmug_f7Ve2jMk";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
