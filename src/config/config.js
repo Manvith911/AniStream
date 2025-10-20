386f435b65bf7772f6a09453077ac9aec4238155
Manvith <manvithudupa073@gmail.com>
2025-10-20 11:03:33 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const config = {
-  serverUrl: import.meta.env.VITE_APP_SERVERURL || "https://animerealm.vercel.app/api",
+  serverUrl: import.meta.env.VITE_APP_SERVERURL || "https://animerealm-backend.onrender.com/api/v1",
   localUrl: import.meta.env.VITE_APP_LOCALURL,
   proxyUrl: import.meta.env.VITE_APP_PROXYURL,
 };
