aba2d1821633182a74274245266a755d52a4d3f7
Manvith <manvithudupa073@gmail.com>
2025-10-22 04:59:14 +0530

    Update ContinueWatching.jsx

--- src/layouts/ContinueWatching.jsx
+++ src/layouts/ContinueWatching.jsx
@@ -14,15 +14,23 @@ const ContinueWatching = () => {
   if (watchList.length === 0) return null;
 
   return (
-    <div className="mt-10">
+    <div className="mt-10 px-2">
       <Heading>Continue Watching</Heading>
-      <div className="flex justify-around flex-wrap gap-4">
+      <div className="flex overflow-x-auto gap-4 pb-4 scrollbar-hide">
         {watchList.map((anime) => (
-          <div key={anime.id} className="flw-item">
-            <Link to={`/watch/${anime.id}`}>
-              <Image data={anime} />
-              <p className="text-center text-sm mt-2 text-gray-300">
-                Episode {anime.episode}
+          <div
+            key={anime.id}
+            className="min-w-[130px] sm:min-w-[160px] flex-shrink-0"
+          >
+            <Link to={`/watch/${anime.id}?ep=${anime.episode}`}>
+              <div className="relative">
+                <Image data={anime} />
+                <div className="absolute bottom-1 left-1 bg-black/70 px-2 py-[2px] text-xs rounded">
+                  Ep {anime.episode}
+                </div>
+              </div>
+              <p className="text-gray-300 text-xs mt-2 line-clamp-2">
+                {anime.title}
               </p>
             </Link>
           </div>
