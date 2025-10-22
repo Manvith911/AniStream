4999a53a9c8e8c86a63427aa05de7ef5b17356cf
Manvith <manvithudupa073@gmail.com>
2025-10-22 16:10:23 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.jsx";
@@ -6,11 +6,12 @@ import "./index.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
-const queryCLient = new QueryClient();
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-  <QueryClientProvider client={queryCLient}>
+  <QueryClientProvider client={queryClient}>
     <StrictMode>
       <ToastContainer
         position="top-right"
@@ -25,7 +26,9 @@ createRoot(document.getElementById("root")).render(
         theme="dark"
       />
       <BrowserRouter>
-        <App />
+        <AuthProvider>
+          <App />
+        </AuthProvider>
       </BrowserRouter>
     </StrictMode>
   </QueryClientProvider>
