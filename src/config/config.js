8f8ce637b04213fca1b13761adac48666edd063e
Manvith <manvithudupa073@gmail.com>
2025-10-24 05:17:47 +0530

    Delete src/config directory

--- src/config/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  serverUrl: import.meta.env.VITE_APP_SERVERURL || "https://www.animerealm.in/api",
-  localUrl: import.meta.env.VITE_APP_LOCALURL,
-  proxyUrl: import.meta.env.VITE_APP_PROXYURL,
-};
-
-export default config;
