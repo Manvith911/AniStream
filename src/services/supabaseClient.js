408ed84749673ad6e9175a5b29834ab974c9693b
Manvith <manvithudupa073@gmail.com>
2025-10-24 04:18:42 +0530

    Update supabaseClient.js

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://kgwtwcchttcamekzqqjx.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_3t9UEKWzWSPxcLKX8Xwmug_f7Ve2jMk";
+const SUPABASE_URL = "https://bjitlknvvjhflpqcoeww.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_Q-4vq83eR29Xgu0ZnZMrHw_Qhx-mTMu";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
