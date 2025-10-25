32bd47c4f24f5ddeeb9e3f0a5e71ede31bbd6136
Manvith <manvithudupa073@gmail.com>
2025-10-25 06:50:31 +0530

    Create Profile.jsx

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,12 @@
+// src/pages/Profile.jsx
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-xl font-semibold">Your Profile</h1>
+      <p className="text-sm text-gray-500 mt-2">Edit your information here.</p>
+    </div>
+  );
+};
+export default Profile;
