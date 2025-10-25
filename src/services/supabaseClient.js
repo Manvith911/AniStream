2c737dde5f62f81eb73b03d7a81474c5343c185a
Manvith <manvithudupa073@gmail.com>
2025-10-25 10:58:56 +0530

    Create supabaseClient.js

---
+++ src/services/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL = "https://bjitlknvvjhflpqcoeww.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_Q-4vq83eR29Xgu0ZnZMrHw_Qhx-mTMu";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
