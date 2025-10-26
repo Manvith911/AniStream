5a7867a98752e9fcfec0720815c4bb36db39c958
Manvith <manvithudupa073@gmail.com>
2025-10-26 08:00:41 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,32 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
 import App from "./App.jsx";
 import "./index.css";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter } from "react-router-dom";
 
-// Create a new React Query client
-const queryClient = new QueryClient();
+const queryCLient = new QueryClient();
 
-// Render the root app
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
+  <QueryClientProvider client={queryCLient}>
+    <StrictMode>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
       <BrowserRouter>
-        <ToastContainer
-          position="top-right"
-          autoClose={2000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="dark"
-        />
         <App />
       </BrowserRouter>
-    </QueryClientProvider>
-  </StrictMode>
+    </StrictMode>
+  </QueryClientProvider>
 );
