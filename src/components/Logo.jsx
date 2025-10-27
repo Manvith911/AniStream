417368678cd22be9905b5c3f58fa245ea1ab235b
Manvith <manvithudupa073@gmail.com>
2025-10-27 19:15:32 +0530

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -9,7 +9,7 @@ const Logo = () => {
         alt="AnimeRealm Logo"
         className="w-6 h-6 object-contain"
       />
-      <h1 className="gradient-text text-xl">𝓐𝓷𝓲𝓶𝓮𝓡𝓮𝓪𝓵𝓶</h1>
+      <h1 className="gradient-text text-xl">𝐀𝐧𝐢𝐦𝐞𝐑𝐞𝐚𝐥𝐦</h1>
     </Link>
   );
 };
