0daf176ecf062c7f697327aeac2314d7d69dd213
Manvith <manvithudupa073@gmail.com>
2025-10-29 12:51:00 +0530

    Create ProfilePage.jsx

---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,10 @@
+const ProfilePage = () => {
+  return (
+    <div className="text-center mt-10 text-white">
+      <h1 className="text-2xl font-bold">User Profile</h1>
+      <p>Coming soon...</p>
+    </div>
+  );
+};
+
+export default ProfilePage;
