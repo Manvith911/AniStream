2f5698a639ea2d28eac1b51efec5da43b13417f7
Manvith <manvithudupa073@gmail.com>
2025-10-29 12:49:08 +0530

    Create useAuth.js

---
+++ src/services/useAuth.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { supabase } from "./supabaseClient";
+
+export const useAuth = () => {
+  const [session, setSession] = useState(null);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const getSession = async () => {
+      const { data } = await supabase.auth.getSession();
+      setSession(data.session);
+      setUser(data.session?.user || null);
+    };
+    getSession();
+
+    const { data: listener } = supabase.auth.onAuthStateChange(
+      (_event, session) => {
+        setSession(session);
+        setUser(session?.user || null);
+      }
+    );
+
+    return () => {
+      listener.subscription.unsubscribe();
+    };
+  }, []);
+
+  const signInWithGoogle = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `${window.location.origin}/auth/callback`,
+      },
+    });
+  };
+
+  const signOut = async () => {
+    await supabase.auth.signOut();
+  };
+
+  return { session, user, signInWithGoogle, signOut };
+};
